2390e2ba1591106961f2deb29ae18b86c1ea433c
bit <bit@bit-PC>
2015-08-24 13:03:51 +0900

    3st

--- src/main/java/com/bit2015/bit2015mall/controller/MainController.java
+++ src/main/java/com/bit2015/bit2015mall/controller/MainController.java
@@ -2,7 +2,7 @@ package com.bit2015.bit2015mall.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+ 
 @Controller
 public class MainController {
 	
